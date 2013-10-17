f094286a4185ab0f9ddef99f59e28927d80ea424
Giedrius <giedrius@bounce.lt>
2013-10-17 15:50:24 +0300

    Clean master

--- lib/NetVerify.php
+++ lib/NetVerify.php
@@ -50,6 +50,8 @@ class NetVerify {
                 $request = $client->post($this->request->getActionUrl(), array(), json_encode($this->request->getParams()) );
                 $request->setHeader('Accept','application/json');
                 $request->setHeader('Content-Type','application/json');
+                $request->getCurlOptions()->set(CURLOPT_SSL_VERIFYHOST, false);
+                $request->getCurlOptions()->set(CURLOPT_SSL_VERIFYPEER, false);
                 $request->setAuth($this->apiMerchantToken, $this->apiSecret);
                 $response = $request->send();
                 $this->response = new Response($response->json());
